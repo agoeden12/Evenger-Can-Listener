f7eda1e7fadcdb3126b6724b078fc7ecf6f7e001
Andrew Goeden <andrewgoeden@gmail.com>
2021-04-22 18:42:11 -0400

    Finished refactor of functions and cleaned up code

--- local_test.py
+++ local_test.py
@@ -23,17 +23,23 @@ class EvengerLogger:
         #         {"can_id": 0x073, "can_mask": 0x1ff, "extended": False}]
         # )]
 
-        msg = Message(is_extended_id=False, arbitration_id=1712, data=[
+        self.msg = Message(is_extended_id=False, arbitration_id=1712, data=[
                       0x00, 0x00, 0x03, 0xB6, 0x98, 0x40, 0x40, 0x89])
-        msg1 = Message(is_extended_id=False, arbitration_id=115, data=[
+        self.msg1 = Message(is_extended_id=False, arbitration_id=115, data=[
                        0x29, 0x27, 0x28, 0x00, 0x00, 0x00, 0x00, 0x00])
 
+    def start_logging(self):
+        # initialVoltage = int("".join(self.decode_msg(busArray[0].recv())[2:4]), 16) / 10.0
+        initialVoltage = int("".join(self.decode_msg(self.msg)[2:4]), 16) / 10.0
+        response = requests.post("http://localhost:5000/initialize", data=json.dumps({"initialVoltage": initialVoltage}), headers={'Content-type': 'application/json', 'Accept': 'text/plain'})
+        print(response)
+
         while True:
-            # messages = busArray[0].recv() + busArray[1].recv()
-            messageData = self.decode_msg(msg) + self.decode_msg(msg1)
+            # messages = self.busArray[0].recv() + self.busArray[1].recv()
+            messageData = self.decode_msg(self.msg) + self.decode_msg(self.msg1)
             self.process_data(messageData)
             time.sleep(.5)
-
+        
     async def sendData(self, body):
         async with websockets.connect("ws://localhost:5000/point") as websocket:
             await websocket.send(body)
@@ -41,19 +47,15 @@ class EvengerLogger:
 
     def decode_msg(self, msg):
         data = binascii.hexlify(msg.data)
-        bytes0 = [data[i:i+2].decode('utf-8') for i in range(0, len(data), 2)]
+        bytes0 = [data[i:i+2].decode('utf-8') for i in range(0, len(data), 2)] # May need to remove .decode('utf-8') for the actual messages
         return bytes0
 
     def process_data(self, data):
-        currentBytes = (data[0] + data[1])
-        current = int(currentBytes, 16) / 10.0
-
-        voltageBytes = (data[2] + data[3])
-        voltage = int(voltageBytes, 16) / 10.0
-
-        highestCell = int(data[8], 16) / 10.0
-        lowestCell = int(data[9], 16) / 10.0
-        averageCell = int(data[10], 16) / 10.0
+        current = int("".join(data[0:2]), 16) / 10.0 # Current is split across bytes 0 & 1
+        voltage = int("".join(data[2:4]), 16) / 10.0 # Voltage is split across bytes 2 & 3
+        highestCell = int(data[8], 16) / 10.0 # Highest Cell Voltage
+        lowestCell = int(data[9], 16) / 10.0 # Lowest Cell Voltage
+        averageCell = int(data[10], 16) / 10.0 # Average Cell Voltage
 
         body = json.dumps({"voltage": voltage, "current": current, "highestCell": highestCell,
                           "lowestCell": lowestCell, "averageCell": averageCell})
@@ -61,9 +63,5 @@ class EvengerLogger:
 
 
 if __name__ == "__main__":
-    initialVoltage = 98
-    response = requests.post("http://localhost:5000/initialize", data=json.dumps(
-        {"initialVoltage": initialVoltage}), headers={'Content-type': 'application/json', 'Accept': 'text/plain'})
-    print(response)
-
     logger = EvengerLogger()
+    logger.start_logging()
